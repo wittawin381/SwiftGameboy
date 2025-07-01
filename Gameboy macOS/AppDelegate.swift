83c5a721f5682053d878160a6cc5b38578cd86e6
wittawin381 <tete.wittawin@gmail.com>
2025-07-01 21:05:57 +0700

    Initial Commit

---
+++ Gameboy macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Gameboy macOS
+//
+//  Created by Wittawin Muangnoi on 1/7/2568 BE.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
